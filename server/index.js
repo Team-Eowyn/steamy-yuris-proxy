beef5fb24d465b7861c3415935733f9734c9c171
Yuri Kim <yurikim123@gmail.com>
2020-06-17 16:57:38 -1000

    added newrelic file, changed connection to local announcements only

--- server/index.js
+++ server/index.js
@@ -7,43 +7,46 @@ const bodyParser = require('body-parser');
 
 app.use(express.static(`${__dirname}/../public`));
 
-const mediaProxy = createProxyMiddleware(
-  {
-    target: 'http://3.23.132.230:8000',
-    changeOrigin: true,
-  },
-);
+require('newrelic');
 
-const sidebarProxy = createProxyMiddleware({
-  target: 'http://52.14.114.30:1992',
-  changeOrigin: true,
-});
+// const mediaProxy = createProxyMiddleware(
+//   {
+//     target: 'http://3.23.132.230:8000',
+//     changeOrigin: true,
+//   },
+// );
+
+// const sidebarProxy = createProxyMiddleware({
+//   target: 'http://52.14.114.30:1992',
+//   changeOrigin: true,
+// });
 
 const announcementsProxy = createProxyMiddleware(
   {
-    target: 'http://3.17.138.9:8080',
+    // target: 'http://3.17.138.9:8080',
+    target: 'http://localhost:8080',
     changeOrigin: true,
   },
 );
 
-const reviewsProxy = createProxyMiddleware(
-  {
-    target: 'http://54.67.101.150:4200',
-    changeOrigin: true,
-  },
-);
+// const reviewsProxy = createProxyMiddleware(
+//   {
+//     target: 'http://54.67.101.150:4200',
+//     changeOrigin: true,
+//   },
+// );
 
-// proxy to media service
-app.use(
-  '/media',
-  mediaProxy,
-);
+// // proxy to media service
+// app.use(
+//   '/media',
+//   mediaProxy,
+// );
 
-// proxy to siderbar service
-app.use(
-  '/mainBody',
-  sidebarProxy,
-);
+// // proxy to siderbar service
+// app.use(
+//   '/mainBody',
+//   sidebarProxy,
+// );
 
 // proxy to announcements service for random game
 app.use(
@@ -63,12 +66,17 @@ app.use(
   announcementsProxy,
 );
 
-
-// proxy to reviews service
 app.use(
-  '/api/reviews/:id',
-  reviewsProxy,
+  '/game',
+  announcementsProxy,
 );
+
+
+// // proxy to reviews service
+// app.use(
+//   '/api/reviews/:id',
+//   reviewsProxy,
+// );
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
